3e6c5e6286303378dd310958c03d8f289b9a46f4
Pranata <94069522+tatah32@users.noreply.github.com>
2022-01-25 20:55:37 +0700

    Update grupbot.js

--- shop/grupbot.js
+++ shop/grupbot.js
@@ -2,17 +2,7 @@
 const gcbotwa = () =>{
 	return`Join Aja Semua Fitur Bot Bisa Digunakan !
 
-1. *Alphabot Support*
-_https://chat.whatsapp.com/EU890BcXjyBDkNaUT5WmYV_
-2. *Alphabot Support 2*
-_https://chat.whatsapp.com/E8NExJwIbhBJYzssfqJNsE_
-3. *Alphabot Support 3*
-_https://chat.whatsapp.com/KCSqHTky1apG7ApePsfiPy_
-4. *Alphabot Support 4* 
-_https://chat.whatsapp.com/KwmvHr7VMFj7r5ry9xmMsU_
-
-Jika ada link yang ke reset, silahkan hubungi
-owner untuk meminta link yang baru
+1. _https://chat.whatsapp.com/Ev930hwkdnrAON0V6za57c_
 `
 }
 exports.gcbotwa = gcbotwa
