89b2c8da8348e3f904b1ae32389164bcb3a6e4ec
Pranata <94069522+tatah32@users.noreply.github.com>
2022-01-25 20:56:36 +0700

    Update allpayment.js

--- shop/allpayment.js
+++ shop/allpayment.js
@@ -1,29 +1,5 @@
 const allpayment = (prefix) => {
-return `PAYMENT BY ZEEONE OFC
-
-*🏧 BANK*
-
-1. BRI 
-	ㅁ 8881 0887 4350 47326
-2. BCA 
-	ㅁ 3901 0887 4350 47326
-3. BNI 
-	ㅁ 881 0887 4350 47326
-4. PERMATA
-	ㅁ 852 8887 4350 47326
-	
-*💰 E-MONEY*
-
-1. GOPAY
-	ㅁ 0887 4350 47326
-2. OVO
-	ㅁ 0887 4350 47326
-3. DANA
-	ㅁ 0887 4350 47326
-	
-Semuanya atas nama RI****DI
-
-*📲 PULSA*
+return `PAYMENT BY Tata
 
 Tidak tersedia
 	
